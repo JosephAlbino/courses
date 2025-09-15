129f369248982e44718e1e99ccd0576530bd3e04
Joseph L. Albino <jos.albinop@gmail.com>
2025-09-15 02:50:09 -0500

    subiendo clases del 1 al 6

---
+++ python/platzi-curso-de-python/scripts/clase4.py
@@ -0,0 +1,19 @@
+name = 'Joseph'
+last_name = '      Albino P   '
+
+print(5 * name) # Se multiplica 5 veces el nombre 
+
+print(type(name)) # type sirve para ver que tipo de variables o dato es
+
+print(name + ' ' + last_name) # Imprime el nombre y apellido juntos
+
+print(len(name)) # Muestra cuantos caracteres tiene la varaible
+print(len(last_name)) 
+
+print(type(len(name))) # Aqui vemos que si vemos que tipo de variables es Len, nos da un int
+
+print(name.lower()) # Convierte todo el texto a minuscula
+
+print(name.upper()) # Convierte todo el texto a Mayuscula
+
+print(last_name.strip()) # Elimina los espcacios en blanco a ambos lados izquierda y derecha
---
+++ python/platzi-curso-de-python/scripts/clase5.py
@@ -0,0 +1,15 @@
+x=10
+y=5.678
+z=1.2e6
+
+print(x)
+print(type(y))
+print(type(z))
+print(x+y)
+
+# Datos Booleanos
+is_true = True
+is_false = False
+
+print(is_true)
+print(type(is_true))
